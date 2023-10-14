b75fa74e1b03ff3cb636069e2d4999f8a067de8e
dev-status-200 <dev.status.200@gmail.com>
2023-10-14 19:40:07 +0500

    update

--- components/layouts/SignUp.js
+++ components/layouts/SignUp.js
@@ -1,24 +1,53 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Spinner } from "react-bootstrap";
 import { HiOutlineMail, HiEye } from "react-icons/hi";
-import { FcGoogle } from "react-icons/fc";
-import { AiFillApple } from "react-icons/ai";
+import { message } from 'antd';
 import Link from 'next/link';
+import { AiOutlineUser } from "react-icons/ai";
+import { IoCallOutline } from "react-icons/io5";
 
 const Login = () => {
 
+    const [messageApi, contextHolder] = message.useMessage();
+
     const [load, setLoad] = useState(false);
     const [reveal, setReveal] = useState(false);
-    const [username, setUsername] = useState("");
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPass, setConfirmPass] = useState("");
+
+    const msg = (type, text) => {
+        messageApi.open({
+          type: type,
+          content: text,
+        });
+      };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if(password!=confirmPass){
+            msg('error', 'Passwords not same!')
+        } else {
+            setLoad(true)
+            console.log({
+                firstName,
+                lastName,
+                email,
+                phone,
+                password,
+                confirmPass
+            })
+        }
     }
-    
+
   return (
     <div className='login-styles'>
+    {contextHolder}
     <Row>
         <Col md={7} className='login-bg'></Col>
         <Col md={5}>
@@ -27,48 +56,51 @@ const Login = () => {
                 <h1 className='fw-900'>Sign-Up</h1>
                 <p className='grey2-txt mt-3'>Enter following fields     for signup</p>
                 <form onSubmit={handleSubmit}>
-                <div>
-                    <div className='mb-4'>
-                        <div className='mb-2 fs-20'>First Name</div>
-                        <input className='login-inp' required placeholder='Joe' value={username} onChange={(e)=>setUsername(e.target.value)} />
+                <div style={{maxHeight:'50vh', overflowY:'auto'}} className='py-3'>
+                    <div className='mb-2'>
+                        <div className='mb-1'>First Name</div>
+                        <input className='login-inp' required placeholder='Joe' value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
+                        <AiOutlineUser className='username-img cur' />
+                    </div>
+                    <div className='mt-2'>
+                        <div className='mb-1'>Last Name</div>    
+                        <input className='login-inp' placeholder='Doe' required value={lastName} onChange={(e)=>setLastName(e.target.value)} />
+                        <AiOutlineUser className='username-img cur' />
+                    </div>
+                    <div className='mt-2'>
+                        <div className='mb-1'>Email address</div>    
+                        <input className='login-inp' placeholder='example@.com' type="email" required value={email} onChange={(e)=>setEmail(e.target.value)} />
                         <HiOutlineMail className='username-img cur' />
                     </div>
-                    <div className='mt-4'>
-                        <div className='mb-2'>Last Name</div>    
-                        <input className='login-inp' placeholder='Doe' required type={reveal?'text':'password'} value={password} onChange={(e)=>setPassword(e.target.value)} />
-                        <HiEye className='username-img cur' />
+                    <div className='mt-2'>
+                        <div className='mb-1'>Phone Number</div>    
+                        <input className='login-inp' placeholder='923360222373' required value={phone} onChange={(e)=>setPhone(e.target.value)} />
+                        <IoCallOutline className='username-img cur' />
                     </div>
-                    <div className='mt-4'>
-                        <div className='mb-2'>Phone Number</div>    
-                        <input className='login-inp' placeholder='example@.com' required type={reveal?'text':'password'} value={password} onChange={(e)=>setPassword(e.target.value)} />
-                        <HiEye className='username-img cur' />
+                    <div className='mt-2'>
+                        <div className='mb-1'>Password</div>    
+                        <input className='login-inp' placeholder='joanedow123' required type={reveal?'text':'password'} value={password} onChange={(e)=>setPassword(e.target.value)} />
+                        <HiEye className='username-img cur' onClick={()=>setReveal(!reveal)} />
                     </div>
-                    <div className='mt-4'>
-                        <div className='mb-2'>Email address</div>    
-                        <input className='login-inp' placeholder='example@.com' required type={reveal?'text':'password'} value={password} onChange={(e)=>setPassword(e.target.value)} />
-                        <HiEye className='username-img cur' />
-                    </div>
-                    <div className='mt-4'>
-                        <button type='submit' className='btn-login'>
-                            {load?<Spinner animation="border" className='mx-3' size='sm' variant="light" />:'Login'}
-                        </button>
-                    </div>
-                    <div style={{width:"70%"}} className='my-4 px-5'>
-                    <Row>
-                        <Col md={5}><hr/></Col>
-                        <Col md={2} className='text-center' style={{position:"relative", top:5}}>OR</Col>
-                        <Col md={5}><hr/></Col>
-                    </Row>
+                    <div className='mt-2'>
+                        <div className='mb-1'>Confirm Password</div>    
+                        <input className='login-inp' placeholder='joanedow123' required type={reveal?'text':'password'} value={confirmPass} onChange={(e)=>setConfirmPass(e.target.value)} />
+                        <HiEye className='username-img cur' onClick={()=>setReveal(!reveal)} />
                     </div>
                 </div>
+                    <div className='mt-4'>
+                        <button type='submit' className='btn-login'>
+                            {load?<Spinner animation="border" className='mx-3' size='sm' variant="light" />:'Sign up'}
+                        </button>
+                    </div>
                 </form>
                 </div>
             </Container>
             <Container className='bottom-container px-5'>
                 <div>
-                    <p className='grey2-txt' style={{fontSize:13}}>Enter your credentials to access your account</p>
+                    <p className='grey2-txt' style={{fontSize:13}}>Enter your details to continue</p>
                 </div>
-                <Link href={"/signup"} className='no-link orange-txt pb-3'>Register Here</Link>
+                <Link href={"/login"} className='no-link orange-txt pb-3'>Login Instead</Link>
             </Container>
         </Col>
     </Row>
