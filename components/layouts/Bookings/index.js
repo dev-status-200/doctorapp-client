1e9d1d02c9fe7a0b25ef5111c7f06e2110946ad4
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2024-01-20 02:12:02 +0500

    minor update in panel container

--- components/layouts/Bookings/index.js
+++ components/layouts/Bookings/index.js
@@ -7,9 +7,14 @@ const Bookings = ({ appointments }) => {
     <React.Fragment>
       <Row>
         <Col md={2}>
-          <div className="panel-container ">
-            <span>ssss</span>
-            <hr/>
+          <div className="panel-container p-3">
+            <ul>
+              <li>Option 1</li>
+              <li>Option 2</li>
+              <li>Option 3</li>
+              <li>Option 4</li>
+              <li>Option 5</li>
+            </ul>
           </div>
         </Col>
         <Col md={10}>
